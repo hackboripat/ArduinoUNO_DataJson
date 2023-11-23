7110529c0207fbe52c7e2f6c47b5f589176470ec
hackthtp <hakthatphanom@hotmail.com>
2023-11-24 00:41:34 +0700

    first commit

---
+++ UNO_DHT22_PZEM-004T/UNO_DHT22_PZEM-004T.ino
@@ -0,0 +1,71 @@
+#include <PZEM004Tv30.h>
+#include "DHT.h"
+#include <ArduinoJson.h>
+
+#define dht22_pin 4     // Digital pin connected to the DHT sensor
+#define DHT22TYPE DHT22   // DHT 22  (AM2302), AM2321
+
+
+PZEM004Tv30 pzem(2, 3);
+DHT dht(dht22_pin, DHT22TYPE);
+
+DynamicJsonDocument doc(1024);
+
+float temperature = 0; float humidity = 0;
+
+float voltage = 0; float current = 0;
+
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+
+  pzem_read();
+  dht22_read();
+
+  doc["temp"] = voltage;
+  doc["rH"]   = humidity;
+  doc["voltage"] = voltage;
+  doc["current"] = current;
+
+  serializeJson(doc, Serial);
+
+  delay(300);
+}
+
+void pzem_read()
+{
+  float voltage_in = pzem.voltage();    
+  if(voltage != NAN){
+    voltage = voltage_in;
+  } else {
+    voltage = 0;
+  }
+
+  float current_in = pzem.current();
+  if(current != NAN){
+    current = current_in;
+  } else {
+    current = 0;
+  }
+}
+
+void dht22_read()
+{
+  float h = dht.readHumidity(); 
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (!isnan(h) || !isnan(t)) {
+
+    temperature = t; current  = h;
+  }else{
+
+    temperature = 0; current  = 0;    
+  }
+
+  
+}
